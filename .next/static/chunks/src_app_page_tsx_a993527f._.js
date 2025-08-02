0e460166d8362c4785ca3c0bcf49cd0315c1ebb2
n1pp3t <84445491+n1pp3t@users.noreply.github.com>
2025-08-02 21:57:14 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754161033631

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f296f8f0._.js",
+  "static/chunks/node_modules_33ec2b7e._.js"
+],
+    source: "dynamic"
+});
