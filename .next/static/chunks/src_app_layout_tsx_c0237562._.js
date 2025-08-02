538e73227af5b15504b3ea932a5ecc49c98f50c0
n1pp3t <84445491+n1pp3t@users.noreply.github.com>
2025-08-02 21:56:29 +0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1754160988868

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_components_NotificationCenter_tsx_4a87accd._.js"
+],
+    source: "dynamic"
+});
